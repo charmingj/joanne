592d3fb893004d5a1bdb8b2a6790750ec55c6685
joanne.huang <joanne.huang@hotmail.com>
2015-03-12 01:18:18 +0800

    update commet to 45s

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
 	def home
 		# get public messages
-		@comments = Comment.where(subject: 0).order('created_at DESC').first(10)
+		@comments = Comment.where(subject: 0).order('created_at DESC').first(45)
 	end
 
 	
